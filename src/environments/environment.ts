4bc29a26b3f4689163959a5d060a09154eec7379
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2026-03-26 12:55:54 -0500

    feat: configure production environment for AWS deployment (api.cyclonet.com.co)

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    BASE_URL_AUTHORIZA: 'http://localhost:3000',
+    BASE_URL_FACTONET: 'http://localhost:3003',
+    BASE_URL_INOUT: 'http://localhost:3001',
+    authorizaApiUrl: 'http://localhost:3000'
+  };
